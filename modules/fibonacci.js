b2b3b4deb4f67e0175d82a35b24030fccce77f02
Bill Wright <bill@wwwright.com>
2023-07-09 12:42:52 -0600

    Cleaned up fibonacci demo

--- modules/fibonacci.js
+++ modules/fibonacci.js
@@ -1,6 +1,6 @@
 function fibonacci(number) {
     // First version:
-    // return 0;
+    return 0;
 
     // Next version:
     // if (number === 0) return 0;
@@ -18,12 +18,14 @@ function fibonacci(number) {
     // return fibonacci(number-1) + fibonacci(number-2);
 }
 
-function fibonacci(element) {
-    const sequence = [0, 1];
-    for (i = 2; i <= element; i++) {
-        sequence[i] = sequence[i - 2] + sequence[i - 1];
-    }
-    return sequence[element];
-}
+// function fibonacci(element) {
+//     if (element < 0) throw new Error('Input number must not be negative');
+//     if (!Number.isInteger(element)) throw new Error('Input must be an integer');
+//     const sequence = [0, 1];
+//     for (i = 2; i <= element; i++) {
+//         sequence[i] = sequence[i - 2] + sequence[i - 1];
+//     }
+//     return sequence[element];
+// }
 
 module.exports = fibonacci;
