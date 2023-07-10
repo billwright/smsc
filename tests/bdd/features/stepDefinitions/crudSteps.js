8f6806b4b018c67f56e34856498507b4750f17e6
Bill Wright <bill@wwwright.com>
2023-07-09 22:58:00 -0600

    Added tracking of creating objects and deleting

--- tests/bdd/features/stepDefinitions/crudSteps.js
+++ tests/bdd/features/stepDefinitions/crudSteps.js
@@ -1,10 +1,27 @@
-const pactum = require("pactum");
-const { Given, When, Then, Before, After } = require("@cucumber/cucumber");
-require("dotenv").config();
+const pactum = require('pactum');
+const { Given, When, Then, Before, After } = require('@cucumber/cucumber');
+require('dotenv').config();
 
 const BASE_URL = process.env.BASE_URL;
 
 let spec;
+
+Before(function () {
+    console.log('In the Before function');
+});
+
+After(function () {
+    console.log('In the After function');
+    // Delete all created objects
+    for (let key in createdObjects) {
+        for (let i = 0; i < createdObjects[key].length; i++) {
+            let id = createdObjects[key][i];
+            let path = `${key}/${id}`;
+            console.log(`Deleting ${path}...`);
+            deleteAtPath(path);
+        }
+    }
+});
 
 function convertToJson(entity) {
     const json = {};
@@ -25,11 +42,11 @@ function generateNewRandom() {
 
 createdObjects = {};
 function rememberCreatedObject(path, objectId) {
-    if (! createdObjects.hasOwnProperty(path)) {
+    if (!createdObjects.hasOwnProperty(path)) {
         createdObjects[path] = [];
     }
     createdObjects[path].push(objectId);
-    console.log('Created objects are:', createdObjects)
+    console.log('Created objects are:', createdObjects);
 }
 
 function populate(value) {
@@ -37,27 +54,35 @@ function populate(value) {
 }
 
 Given(/^the server is running$/, async function () {
-    await pactum.spec()
-        .get(BASE_URL)
-        .expectStatus(200);
+    await pactum.spec().get(BASE_URL).expectStatus(200);
 });
 
 When(
-  /^I (post|put) the following data to the endpoint "(.*)":$/,
-  function (action, endpoint, dataTable) {
-    entities = dataTable.hashes();
-    firstEntity = entities[0];   
-    
-    spec = pactum.spec();
-    spec.withJson(convertToJson(firstEntity));
-    const fullURL = BASE_URL+endpoint
-    console.log('The full URL:', fullURL);
-    spec[action](fullURL);
-  }
+    /^I (post|put) the following data to the endpoint "(.*)":$/,
+    function (action, endpoint, dataTable) {
+        entities = dataTable.hashes();
+        firstEntity = entities[0];
+
+        spec = pactum.spec();
+        spec.withJson(convertToJson(firstEntity));
+        const fullURL = BASE_URL + endpoint;
+        console.log('The full URL:', fullURL);
+        spec[action](fullURL);
+    }
 );
 
+// https://github.com/pactumjs/pactum/discussions/76
+async function deleteAtPath(path) {
+    await pactum.spec()
+    .delete(BASE_URL + path)
+    .expectStatus(200);
+}
+
+When(/^I delete the object at url "(.*)"$/, deleteAtPath);
+
 Then(/^the (post|put) succeeds$/, async function (action) {
-  await spec.toss();
-  spec.response().should.have.status(201);
-  rememberCreatedObject(spec._response.req.path, spec._response.json.id)
+    await spec.toss();
+    spec.response().should.have.status(201);
+    // https://github.com/pactumjs/pactum/discussions/81
+    rememberCreatedObject(spec._response.req.path, spec._response.json.id);
 });
